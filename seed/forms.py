f045e7e3b2fb8434931c6140245526f7b4a5754f
Jaeyeon Kim <amelia1023@naver.com>
2022-05-31 16:00:35 +0900

    seed create bootstrap

--- seed/forms.py
+++ seed/forms.py
@@ -1,3 +1,4 @@
+from tkinter import Widget
 from django import forms
 from django.db.models import fields
 from .models import Seed
@@ -6,3 +7,41 @@ class SeedForm(forms.ModelForm):
     class Meta:
         model = Seed
         fields = ['intro_num', 'family', 'genus', 'used_scientific_name', 'plant_name', 'microscope', 'seed_length', 'seed_length_error', 'seed_width', 'seed_width_error', 'note']
+        widgets = {
+            'intro_num' : forms.TextInput(attrs={
+                "class" : "form-control",
+                "placeholder" : "ex) 2020-001492"
+            }),
+            'used_scientific_name' : forms.TextInput(attrs={
+                "class" : "form-control",
+                "placeholder" : "ex) Aster maackii Regel"
+            }),
+            'plant_name' : forms.TextInput(attrs={
+                "class" : "form-control",
+                "placeholder" : "ex) 좀개미취"
+            }),
+            'microscope' : forms.CheckboxInput(attrs={
+                "class" : "form-check-input",
+                "style" : "margin-left: 15px;"
+            }),
+            'seed_length' : forms.NumberInput(attrs={
+                "class" : "form-control",
+                "placeholder" : "ex) 2.71"
+            }),
+            'seed_length_error' : forms.TextInput(attrs={
+                "class" : "form-control",
+                "placeholder" : "ex) 0.05"
+            }),
+            'seed_width' : forms.TextInput(attrs={
+                "class" : "form-control",
+                "placeholder" : "ex) 1.01"
+            }),
+            'seed_width_error' : forms.TextInput(attrs={
+                "class" : "form-control",
+                "placeholder" : "ex) 0.02"
+            }),
+            'note' : forms.Textarea(attrs={
+                "class" : "form-control",
+                "placeholder" : "ex) 1차"
+            })
+        }
