1d7f2b6eb50341191f54933edeaac15231a656a8
Jaeyeon Kim <amelia1023@naver.com>
2022-06-02 14:25:31 +0900

    image upload

--- seed/admin.py
+++ seed/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Seed
+from .models import Seed, SeedImage
 
 # Register your models here.
 
 class SeedAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Seed._meta.fields]
 
+class SeedImageAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in SeedImage._meta.fields]
+
 admin.site.register(Seed, SeedAdmin)
+admin.site.register(SeedImage, SeedImageAdmin)
--- seed/views.py
+++ seed/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from .models import Seed
+from .models import Seed, SeedImage
 from .forms import SeedForm
 # Create your views here.
 
 def new(request):
     if request.method == 'POST': # POST 요청일때 새로운 seed 저장
         if request.user.is_authenticated:
-            form = SeedForm(request.POST, request.FILES)
+            form = SeedForm(request.POST)
             if form.is_valid():
+                # 종자 생성
                 seed = form.save(commit=False)
                 seed.save()
+
+                # 이미지 생성
+                image_set = request.FILES
+                for image_data in image_set.getlist('image'):
+                    SeedImage.objects.create(seed=seed, image=image_data)
+
                 return redirect('home') # seed 생성 후 home 페이지로 리다이렉트
             else:
                 print("form is invalid")
@@ -32,6 +39,12 @@ def edit(request, pk):
         if form.is_valid():
             seed = form.save(commit=False)
             seed.save()
+
+            # 이미지 추가
+            image_set = request.FILES
+            for image_data in image_set.getlist('image'):
+                SeedImage.objects.create(seed=seed, image=image_data)
+                
             return redirect('detail', pk=seed.pk)
         else:
             print("form is invalid")
